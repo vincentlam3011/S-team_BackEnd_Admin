9c9d1f080879b7debc6f9d5599fc6a8ea4967ddf
quocanh969 <tranquocanh858@gmail.com>
2020-06-28 16:48:22 +0700

    add 2 report api

--- models/reportModel.js
+++ models/reportModel.js
@@ -1,5 +1,30 @@
 var db = require('../utils/db');
 
 module.exports = {
-    
+    getReportsList:(status, queryName) => {
+        if(queryName === '' && status === 3) {
+            return db.query(`select r.*, u1.fullname as user1_name, u2.fullname as user2_name from reports as r, users as u1, users as u2 where u1.id_user = r.id_user1 and u2.id_user = r.id_user2 order by report_date desc`);
+        }
+        else if(status === 3 && queryName !== '') {
+            return db.query(`select r.*, u1.fullname as user1_name, u2.fullname as user2_name from reports as r, users as u1, users as u2 where u1.fullname LIKE '%${queryName}%' and u1.id_user = r.id_user1 and u2.id_user = r.id_user2 order by report_date desc`)
+        }
+        else if(status !== 3 && queryName === '') {
+            return db.query(`select r.*, u1.fullname as user1_name, u2.fullname as user2_name from reports as r, users as u1, users as u2 where r.status = ${status} and u1.id_user = r.id_user1 and u2.id_user = r.id_user2 order by report_date desc`)
+        }
+        else {
+            return db.query(`select r.*, u1.fullname as user1_name, u2.fullname as user2_name from reports as r, users as u1, users as u2 where u1.fullname LIKE '%${queryName}%' and u1.id_user = r.id_user1 and u2.id_user = r.id_user2 and r.status = ${status} order by report_date desc`)
+        }
+    },
+    setReportStatus: (id_report, status, solution) => {
+        if(status === 0) {
+            return db.query(`update reports set status = 0, solution = null where id_report = ${id_report}`)
+        }
+        else if(status === 1) {
+            return db.query(`update reports set status = 1, solution = N'${solution}' where id_report = ${id_report}`)
+        }
+        else {
+            // donothing
+            return ;
+        }
+    }
 }
--- routes/reports.js
+++ routes/reports.js
@@ -3,5 +3,30 @@ var router = express.Router();
 var { response, DEFINED_CODE } = require('../config/response');
 var reportModel = require('../models/reportModel');
 
+router.post('/getReportsList', (req, res, next) => {    
+    let page = Number.parseInt(req.body.page);
+    let take = Number.parseInt(req.body.take);
+    let status = Number.parseInt(req.body.status);
+    let queryName = req.body.queryName;
+    reportModel.getReportsList(status, queryName)
+        .then(data => {
+            let finalData = data.slice(take*(page - 1), take*page);
+            response(res, DEFINED_CODE.GET_DATA_SUCCESS,{list: finalData, total: data.length, page: page});
+        }).catch(err => {
+            response(res, DEFINED_CODE.GET_DATA_FAIL, err);
+        })
+})
+
+router.post('/setReportStatus', (req, res, next) => {
+    let id_report = Number.parseInt(req.body.id_report);
+    let status  = Number.parseInt(req.body.status);
+    let solution = req.body.solution;
+    reportModel.setReportStatus(id_report, status, solution)
+        .then(data => {
+            response(res, DEFINED_CODE.EDIT_PERSONAL_SUCCESS, {RowChanged: data.RowChanged});
+        }).catch(err => {
+            response(res, DEFINED_CODE.EDIT_PERSONAL_FAIL, err);
+        })
+})
 
 module.exports = router;
